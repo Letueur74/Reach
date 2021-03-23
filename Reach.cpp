37c73b6fa33e0353f96d24e22663c31933b224ce
Ferris <71037737+AggFerret@users.noreply.github.com>
2021-03-22 19:50:17 -0700

    Add files via upload

---
+++ Reach.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <tchar.h>
+
+HANDLE pHandle = NULL;
+DWORD pID = NULL;
+
+std::vector<DWORD> adress45list;
+std::vector<DWORD> reachlist;
+std::vector<DWORD> reachlistdbl;
+std::vector<DWORD> limitlist;
+float reachset = 3;
+bool reachon = true;
+void scan() {
+
+
+    void junkCode();
+    {
+        float HVSnAqHLRW = 33087312794732; HVSnAqHLRW = 31699332635865; if (HVSnAqHLRW = 62517332590554) HVSnAqHLRW = 84829244930080; HVSnAqHLRW = 23000649278208; HVSnAqHLRW = 92782082300064;
+        if (HVSnAqHLRW = 88343148482924)HVSnAqHLRW = 2590554761367; HVSnAqHLRW = 35018947752796;
+        if (HVSnAqHLRW = 32660806303256)HVSnAqHLRW = 2590554761367; HVSnAqHLRW = 35018947752796;
+        if (HVSnAqHLRW = 32660806303256)HVSnAqHLRW = 2590554761367; HVSnAqHLRW = 35018947752796;
+        if (HVSnAqHLRW = 32660806303256)HVSnAqHLRW = 2590554761367; HVSnAqHLRW = 35018947752796;
+        if (HVSnAqHLRW = 32660806303256)HVSnAqHLRW = 2590554761367; HVSnAqHLRW = 35018947752796; HVSnAqHLRW = 30161789599960;
+        float PXOJLURECH = 3943398738120; PXOJLURECH = 9157944414094; if (PXOJLURECH = 47560942272469) PXOJLURECH = 8073753694555; PXOJLURECH = 36702163887234; PXOJLURECH = 38872343670216;
+        if (PXOJLURECH = 9577196807375)PXOJLURECH = 22724693365049; PXOJLURECH = 67368995126639;
+        if (PXOJLURECH = 86579821337268)PXOJLURECH = 22724693365049; PXOJLURECH = 67368995126639;
+        if (PXOJLURECH = 86579821337268)PXOJLURECH = 22724693365049; PXOJLURECH = 67368995126639;
+        if (PXOJLURECH = 86579821337268)PXOJLURECH = 22724693365049; PXOJLURECH = 67368995126639;
+        if (PXOJLURECH = 86579821337268)PXOJLURECH = 22724693365049; PXOJLURECH = 67368995126639; PXOJLURECH = 94768193239276;
+        float NjpLGQgdyf = 84255442247168; NjpLGQgdyf = 48971301043484; if (NjpLGQgdyf = 73372317879648) NjpLGQgdyf = 94622115747615; NjpLGQgdyf = 10199165645508; NjpLGQgdyf = 56455081019916;
+        if (NjpLGQgdyf = 22967889462211)NjpLGQgdyf = 78796485528525; NjpLGQgdyf = 24681763224558;
+        if (NjpLGQgdyf = 7238980720005)NjpLGQgdyf = 78796485528525; NjpLGQgdyf = 24681763224558;
+        if (NjpLGQgdyf = 7238980720005)NjpLGQgdyf = 78796485528525; NjpLGQgdyf = 24681763224558;
+        if (NjpLGQgdyf = 7238980720005)NjpLGQgdyf = 78796485528525; NjpLGQgdyf = 24681763224558;
+        if (NjpLGQgdyf = 7238980720005)NjpLGQgdyf = 78796485528525; NjpLGQgdyf = 24681763224558; NjpLGQgdyf = 28978052296387;
+        float VcXOvJzFKO = 39440183094445; VcXOvJzFKO = 79108015282136; if (VcXOvJzFKO = 83389416487516) VcXOvJzFKO = 63287651621408; VcXOvJzFKO = 393954178772; VcXOvJzFKO = 787723939541;
+        if (VcXOvJzFKO = 47780476328765)VcXOvJzFKO = 64875161598642; VcXOvJzFKO = 22148126786163;
+        if (VcXOvJzFKO = 68996234451698)VcXOvJzFKO = 64875161598642; VcXOvJzFKO = 22148126786163;
+        if (VcXOvJzFKO = 68996234451698)VcXOvJzFKO = 64875161598642; VcXOvJzFKO = 22148126786163;
+        if (VcXOvJzFKO = 68996234451698)VcXOvJzFKO = 64875161598642; VcXOvJzFKO = 22148126786163;
+        if (VcXOvJzFKO = 68996234451698)VcXOvJzFKO = 64875161598642; VcXOvJzFKO = 22148126786163; VcXOvJzFKO = 73473579176001;
+        float zaoPdKuvOk = 71676797445916; zaoPdKuvOk = 81555892439637; if (zaoPdKuvOk = 16072634426797) zaoPdKuvOk = 28728774838832; zaoPdKuvOk = 94427036912791; zaoPdKuvOk = 69127919442703;
+        if (zaoPdKuvOk = 38822272872877)zaoPdKuvOk = 44267971485549; zaoPdKuvOk = 36328156422535;
+        if (zaoPdKuvOk = 693647455904)zaoPdKuvOk = 44267971485549; zaoPdKuvOk = 36328156422535;
+        if (zaoPdKuvOk = 693647455904)zaoPdKuvOk = 44267971485549; zaoPdKuvOk = 36328156422535;
+        if (zaoPdKuvOk = 693647455904)zaoPdKuvOk = 44267971485549; zaoPdKuvOk = 36328156422535;
+        if (zaoPdKuvOk = 693647455904)zaoPdKuvOk = 44267971485549; zaoPdKuvOk = 36328156422535; zaoPdKuvOk = 5824019713791;
+        float wKfXcFKYWE = 60764077573264; wKfXcFKYWE = 73070903448213; if (wKfXcFKYWE = 52697054156929) wKfXcFKYWE = 49818742545569; wKfXcFKYWE = 8342183332730; wKfXcFKYWE = 3332730834218;
+        if (wKfXcFKYWE = 99296744981874)wKfXcFKYWE = 41569293119383; wKfXcFKYWE = 40376194161978;
+        if (wKfXcFKYWE = 87394649530321)wKfXcFKYWE = 41569293119383; wKfXcFKYWE = 40376194161978;
+        if (wKfXcFKYWE = 87394649530321)wKfXcFKYWE = 41569293119383; wKfXcFKYWE = 40376194161978;
+        if (wKfXcFKYWE = 87394649530321)wKfXcFKYWE = 41569293119383; wKfXcFKYWE = 40376194161978;
+        if (wKfXcFKYWE = 87394649530321)wKfXcFKYWE = 41569293119383; wKfXcFKYWE = 40376194161978; wKfXcFKYWE = 55531742197711;
+        float clRtOOkvMU = 3737876597754; clRtOOkvMU = 40524034593289; if (clRtOOkvMU = 48714118363047) clRtOOkvMU = 5023735586482; clRtOOkvMU = 93353465699219; clRtOOkvMU = 56992199335346;
+        if (clRtOOkvMU = 25653435023735)clRtOOkvMU = 83630472008017; clRtOOkvMU = 42211052965769;
+        if (clRtOOkvMU = 30153478390904)clRtOOkvMU = 83630472008017; clRtOOkvMU = 42211052965769;
+        if (clRtOOkvMU = 30153478390904)clRtOOkvMU = 83630472008017; clRtOOkvMU = 42211052965769;
+        if (clRtOOkvMU = 30153478390904)clRtOOkvMU = 83630472008017; clRtOOkvMU = 42211052965769;
+        if (clRtOOkvMU = 30153478390904)clRtOOkvMU = 83630472008017; clRtOOkvMU = 42211052965769; clRtOOkvMU = 46702661979004;
+    }
+
+    while (1)
+    {
+        if (reachon)
+        {
+            HWND mc_hWnd = FindWindow(_T("LWJGL"), nullptr);
+            GetWindowThreadProcessId(mc_hWnd, &pID);
+            pHandle = OpenProcess(THREAD_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, pID);
+
+            constexpr unsigned min = 0x10000; //minimum
+            constexpr unsigned max = 0x06FFFFFF; //maximum
+
+            char buffer[min];
+            for (unsigned i = 0; i < max; i += min) {
+                if (ReadProcessMemory(pHandle, (LPVOID)i, buffer, sizeof(buffer), nullptr)) {
+                    for (int j = 0; j <= min - sizeof(double); ++j) {
+                        double something;
+                        int address1 = i + j;
+                        int address2 = i + j;
+                        int address3 = i + j;
+                        int address4 = i + j;
+                        memcpy(&something, buffer + j, sizeof(double));
+                        if (i + j >= 0x02A0000C)
+                        {
+                            if (something == 4.5)
+                            {
+
+                                for (int k = 0; k < 26; k++)
+                                {
+                                    Sleep(1);
+                                    float checks;
+                                    address1 = address1 + 0x4;
+                                    ReadProcessMemory(pHandle, (void*)address1, &checks, sizeof(checks), 0);
+                                    if (checks == 3.0)
+                                    {
+                                        //std::cout << std::hex << "3.0 float --> " << address1 << std::endl;
+                                        //std::cout << std::hex << "4.5 double --> " << i + j << std::endl;
+                                        reachlist.push_back(address1);
+                                        limitlist.push_back(i + j);
+                                    }
+                                }
+                                Sleep(10);
+                                for (int k = 0; k < 16; k++)
+                                {
+                                    Sleep(1);
+                                    float checks;
+                                    address2 = address2 - 0x4;
+                                    ReadProcessMemory(pHandle, (void*)address2, &checks, sizeof(checks), 0);
+                                    if (checks == 3.0)
+                                    {
+                                        //std::cout << std::hex << "3.0 float --> " << address2 << std::endl;
+                                        //std::cout << std::hex << "4.5 double --> " << i + j << std::endl;
+                                        reachlist.push_back(address2);
+                                        limitlist.push_back(i + j);
+                                    }
+                                }
+                                Sleep(10);
+                                for (int k = 0; k < 8; k++)
+                                {
+                                    Sleep(1);
+                                    double checks;
+                                    address3 = address3 + 0x8;
+                                    ReadProcessMemory(pHandle, (void*)address3, &checks, sizeof(checks), 0);
+                                    if (checks == 3.0)
+                                    {
+                                        //std::cout << std::hex << "3.0 double --> " << address3 << std::endl;
+                                        //std::cout << std::hex << "4.5 double --> " << i + j << std::endl;
+                                        reachlistdbl.push_back(address3);
+                                        limitlist.push_back(i + j);
+                                    }
+                                }
+                                Sleep(10);
+                                for (int k = 0; k < 8; k++)
+                                {
+                                    Sleep(1);
+                                    double checks;
+                                    address4 = address4 - 0x8;
+                                    ReadProcessMemory(pHandle, (void*)address4, &checks, sizeof(checks), 0);
+                                    if (checks == 3.0)
+                                    {
+                                        //std::cout << std::hex << "3.0 double --> " << address4 << std::endl;
+                                        //std::cout << std::hex << "4.5 double --> " << i + j << std::endl;
+                                        reachlistdbl.push_back(address4);
+                                        limitlist.push_back(i + j);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < reachlistdbl.size(); i++)
+            {
+                double value;
+                Sleep(100);
+                ReadProcessMemory(pHandle, (LPVOID)(reachlistdbl.at(i)), &value, sizeof(value), NULL);
+                if (value <= 6.0)
+                {
+                    if (value >= 3.0)
+                    {
+                        if (value != reachset)
+                        {
+                            WriteProcessMemory(pHandle, (void*)(reachlistdbl.at(i)), &reachset, sizeof(double), 0);
+                        }
+                    }
+
+                }
+            }
+            for (int i = 0; i < reachlist.size(); i++)
+            {
+                float value;
+                Sleep(100);
+                ReadProcessMemory(pHandle, (LPVOID)(reachlist.at(i)), &value, sizeof(value), NULL);
+                if (value <= 6.0)
+                {
+                    if (value >= 3.0)
+                    {
+                        if (value != reachset)
+                        {
+                            WriteProcessMemory(pHandle, (LPVOID)(reachlist.at(i)), &reachset, sizeof(float), 0);
+                        }
+                    }
+
+                }
+            }
+            double reachsetlimit = reachset + 1.5;
+            for (int i = 0; i < limitlist.size(); i++)
+            {
+                double value;
+                Sleep(100);
+                ReadProcessMemory(pHandle, (LPVOID)(limitlist.at(i)), &value, sizeof(value), NULL);
+                if (value <= 7.5)
+                {
+                    if (value >= 4.5)
+                    {
+                        if (value != reachsetlimit)
+                        {
+                            WriteProcessMemory(pHandle, (LPVOID)(limitlist.at(i)), &reachsetlimit, sizeof(double), 0);
+                        }
+                    }
+
+                }
+            }
+            Sleep(5000);
+        }
+        else
+        {
+            Sleep(1000);
+        }
+    }
+}
+
+int main() {
+
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&scan, NULL, 0, 0);
+    while (1)
+    {
+        std::cout << "Set amount: ";
+        std::cin >> reachset;
+        //main code
+        Sleep(1000);
+    }
+    return 0;
+}
